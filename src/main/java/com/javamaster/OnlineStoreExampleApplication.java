5997eaf87cec3fbf8d5e7d14b9a04e50a9edab2d
denys.burda <denys.burda@intellectsoft.net>
2020-05-16 15:25:10 +0300

    init commit

---
+++ src/main/java/com/javamaster/OnlineStoreExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javamaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreExampleApplication.class, args);
+    }
+
+}
